5b26495ed728dc9c2435044661c7177c95f04345
daylenjeez <styssury@gmail.com>
2024-01-08 18:32:56 +0800

    add useLocationBrowser

--- study/useFirstMountState/index.tsx
+++ study/useFirstMountState/index.tsx
@@ -4,9 +4,21 @@ export function useFirstMountState(): boolean {
   const isFirst = useRef(true);
 
   if(isFirst.current) {
+    //闭包导致获取的是上一次的值
     isFirst.current = false;
     return true;
   }
 
   return isFirst.current;
+}
+
+
+const Demo = ()=>{
+  const [count, setCount] = useState(0);
+
+  useEffct(()=>{
+    console.log(count)
+  })
+
+  return <button onClick={()=>setCount(count+1)}>count</button>
 }
--- study/usePrevious/index.tsx
+++ study/usePrevious/index.tsx
@@ -2,7 +2,8 @@ import { useEffect, useRef } from 'react';
 
 export default function usePrevious<T>(state: T): T | undefined {
   const ref = useRef<T>();
-
+  
+  //每次 render 时，都会更新 ref.current 的值，但是获取的 state 值是上一次的，因为 这里捕获的是特定声明周期的 state
   useEffect(() => {
     ref.current = state;
   });
